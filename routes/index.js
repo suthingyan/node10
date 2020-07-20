e25844fa316e1520f6960032c918e1bc28e3f09b
suthingyan <suthingyan67@gmail.com>
2020-07-20 19:08:02 +0630

    first commit

---
+++ routes/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+//const User = require('../model/User');
+var router = express.Router();
+var validator=require('email-validator');
+var Admin=require('../model/Admin');
+const { render } = require('../app');
+var passwordValidator = require('password-validator');
+var schema = new passwordValidator();
+ 
+// Add properties to it
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits()                                 // Must have digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var user=req.session.user?req.session.user.name:'unknown';
+  console.log(user);
+  
+  res.render('index', { user: user });
+});
+router.get('/home',function(req,res,next){
+  res.render('home',{name:"Nay Chi will RS"})
+});
+
+router.get('/signup',function(req,res){
+  res.render('signup');
+});
+router.post('/signup',function(req,res){
+  var admin=new Admin();
+  admin.name=req.body.name;
+  admin.email=req.body.email;
+  admin.password=req.body.pwd;
+  admin.save(function(err,rtn){
+    if(err)throw err;
+    res.redirect('/');
+  });
+});
+router.get('/signin',function(req,res)
+{
+  res.render('signin');
+})
+router.post('/signin',function(req,res)
+{
+  Admin.findOne({email:req.body.email},function(err,rtn){
+    if(err)throw err;
+    if(rtn!=null && Admin.compare(req.body.pwd,rtn.password)){
+      req.session.user={name:rtn.name,email:rtn.email};
+      res.redirect('/')
+    }else{
+      res.redirect('/signin');
+    }
+
+  })
+})
+router.get('/logout',function(req,res){
+  req.session.destroy(function(err,rtn){
+    if(err)throw err;
+    res.redirect('/');
+  })
+})
+router.post('/emaildu',function(req,res){
+  Admin.findOne({email:req.body.email},
+    function(err,rtn){
+      if(err)throw err;
+      if(rtn==null && validator.validate(req.body.email)){
+        res.json({status:false});
+      }
+      else{
+        res.json({status:true});
+      }
+    });
+});
+router.post('/passdu',function(req,res){
+   res.json({status:schema.validate(req.body.password)});
+ })
+module.exports = router;
